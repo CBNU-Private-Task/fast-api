bbca8ed9394754a7d927bdc0364cff4a4a821cb4
Chhun Rotanakkosal <rotanakkosal03@gmail.com>
2025-07-21 14:22:05 +0700

    added middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.router import api_router
 from app.db.database import Base, engine
@@ -15,6 +16,19 @@ app = FastAPI(
     title="My Streaming App",
     description="This is a streaming application built with FastAPI and SQLAlchemy.",
     version="1.0.0",
+)
+
+# CORS Middleware
+origins = [
+    "*",  # Allows all origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_exception_handler(HTTPException, http_exception_handler)
